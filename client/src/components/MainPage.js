fbe06dbebfe5d70a75d8255e90302c6bfd56ee9c
ZHANX326 <zhanx326@mcmaster.ca>
2023-08-13 14:24:24 -0400

    add background effect and improve subfeature popup

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Modal from '@mui/material/Modal';
+import Zoom from '@mui/material/Zoom';
 
 import "../styles/MainPage.css"
 import { features } from '../controllers/FeatureController'
@@ -10,31 +11,39 @@ import SubfeatureList from './SubfeatureList';
 function MainPage() {
     
     const [selectedFeature, setSelectedFeature] = useState("");
+    const [zoom, setZoom] = useState(false);
 
     const handleFeatureSelected = (feature) => {
         setSelectedFeature(feature);
-    }
+        setZoom(true);
+    };
 
     const handleFeatureUnselected = () => {
         setSelectedFeature("");
-    }
+        setZoom(false);
+    };
 
     return (
         <div className='all-feature-container'>
-            <IconGrid options={features} handleCellSelected={handleFeatureSelected}/>
+            <h1>Unlock Your Potential</h1>
+            <div 
+                className='icon-grid-container'
+                style={{ opacity: selectedFeature == "" ? 1 : 0.3}}>
+                <IconGrid options={features} handleCellSelected={handleFeatureSelected}/>
+            </div>
             {features.map(feature => (
                 <Modal
                     open={selectedFeature === feature.value}
                     onClose={handleFeatureUnselected}
-                    // hideBackdrop={true}
                     id='subfeature-popup'
                     key={feature.value}>
-                        <div id='subfeature-list-container'>
-                            <SubfeatureList subfeatures={feature.subfeature}/>
-                        </div>
+                        <Zoom in={zoom}>
+                            <div id='subfeature-list-container'>
+                                <SubfeatureList subfeatures={feature.subfeature}/>
+                            </div>
+                        </Zoom>
                 </Modal>
             ))}
-            
         </div>
     )
 }
