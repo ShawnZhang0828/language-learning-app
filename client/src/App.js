30f2fd6f5c668bc99c7730b8c837f33ed8d66095
ZHANX326 <zhanx326@mcmaster.ca>
2023-10-02 20:45:32 -0400

    apply global font to MUI components

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import './App.css';
-import { userPreferenceContext } from './controllers/PreferenceController';
+import "./App.css";
+import { userPreferenceContext } from "./controllers/PreferenceController";
 
-import SignInForm from './components/SignInForm';
-import PreferencePage from './components/PreferencePage';
-import MainPage from './components/MainPage';
-import VocabularyLibrary from './components/vocabulary/VocabularyLibrary';
-import VocabularyQuiz from './components/vocabulary/VocabularyQuiz';
-import ChatPageParamSetter from './components/conversation/ChatPageParamSetter';
-import ScenarioSelectionPage from './components/conversation/ScenarioSelectionPage';
-import StoryPage from './components/reading/StoryPage';
+import SignInForm from "./components/account/SignInForm";
+import SignUpForm from "./components/account/SignUpForm";
+import PreferencePage from "./components/PreferencePage";
+import MainPage from "./components/MainPage";
+import VocabularyLibrary from "./components/vocabulary/VocabularyLibrary";
+import VocabularyQuiz from "./components/vocabulary/VocabularyQuiz";
+import ChatPageParamSetter from "./components/conversation/ChatPageParamSetter";
+import ScenarioSelectionPage from "./components/conversation/ScenarioSelectionPage";
+import StoryPage from "./components/reading/StoryPage";
 
 function App() {
+  // Use local storage to persist user preference context on page refresh
+  const [userPreference, setUserPreference] = useState(() => {
+    const savedPreference = localStorage.getItem("userPreference");
+    return savedPreference ? JSON.parse(savedPreference) : {};
+  });
 
-    // Use local storage to persist user preference context on page refresh
-    const [userPreference, setUserPreference] = useState(() => {
-        const savedPreference = localStorage.getItem('userPreference');
-        return savedPreference ? JSON.parse(savedPreference) : {};
-    });
+  const theme = createTheme({
+    typography: {
+      fontFamily: `'Catamaran', sans-serif`,
+    },
+  });
 
-    // Update local storage when there is a change in user preference context
-    useEffect(() => {
-        localStorage.setItem('userPreference', JSON.stringify(userPreference));
-    }, [userPreference]);
+  // Update local storage when there is a change in user preference context
+  useEffect(() => {
+    localStorage.setItem("userPreference", JSON.stringify(userPreference));
+  }, [userPreference]);
 
-    return (
-      <userPreferenceContext.Provider value={{ userPreference, setUserPreference }}>
-          <Routes>
-              <Route path="/" element={ <SignInForm /> }/>
-              <Route path="/target-language-pref" element={ <PreferencePage prefName="target language" nextPage={"/original-language-pref"}></PreferencePage> }/>
-              <Route path="/original-language-pref" element={ <PreferencePage prefName="original language" nextPage={"/reason-pref"}></PreferencePage> }/>
-              <Route path="/reason-pref" element={ <PreferencePage prefName="reason" nextPage={"/level-pref"}></PreferencePage> }/>
-              <Route path="/level-pref" element={ <PreferencePage prefName="level" nextPage={"/main"} /> }/>
-              <Route path="/main" element={ <MainPage /> }/>
-              <Route path="/chatbot-chat/:type" element={ <ChatPageParamSetter /> } />
-              <Route path='/role-play' element={ <ScenarioSelectionPage /> }/>
-              <Route path="/vocabulary-library" element={ <VocabularyLibrary /> }/>
-              <Route path="/vocabulary-quiz" element={ <VocabularyQuiz /> }/>
-              <Route path="/story" element={ <StoryPage /> } />
-          </Routes>
+  return (
+    <ThemeProvider theme={theme}>
+      <userPreferenceContext.Provider
+        value={{ userPreference, setUserPreference }}
+      >
+        <Routes>
+          <Route path="/" element={<SignInForm />} />
+          <Route path="/signup" element={<SignUpForm />} />
+          <Route
+            path="/target-language-pref"
+            element={
+              <PreferencePage
+                prefName="target language"
+                nextPage={"/original-language-pref"}
+              ></PreferencePage>
+            }
+          />
+          <Route
+            path="/original-language-pref"
+            element={
+              <PreferencePage
+                prefName="original language"
+                nextPage={"/reason-pref"}
+              ></PreferencePage>
+            }
+          />
+          <Route
+            path="/reason-pref"
+            element={
+              <PreferencePage
+                prefName="reason"
+                nextPage={"/level-pref"}
+              ></PreferencePage>
+            }
+          />
+          <Route
+            path="/level-pref"
+            element={<PreferencePage prefName="level" nextPage={"/main"} />}
+          />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/chatbot-chat/:type" element={<ChatPageParamSetter />} />
+          <Route path="/role-play" element={<ScenarioSelectionPage />} />
+          <Route path="/vocabulary-library" element={<VocabularyLibrary />} />
+          <Route path="/vocabulary-quiz" element={<VocabularyQuiz />} />
+          <Route path="/story" element={<StoryPage />} />
+        </Routes>
       </userPreferenceContext.Provider>
-    );
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- client/src/components/vocabulary/VocabPlaybackControl.js
+++ client/src/components/vocabulary/VocabPlaybackControl.js
@@ -1,230 +1,251 @@
-import React, { useState, useEffect, useContext, useRef } from 'react'
-import { Slider, ToggleButtonGroup, ToggleButton, Snackbar, Alert } from '@mui/material'
-import LooksOneIcon from '@mui/icons-material/LooksOne';
-import LooksTwoIcon from '@mui/icons-material/LooksTwo';
-import Looks3Icon from '@mui/icons-material/Looks3';
-import Looks4Icon from '@mui/icons-material/Looks4';
-import Looks5Icon from '@mui/icons-material/Looks5';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import PauseIcon from '@mui/icons-material/Pause';
-import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import LoopIcon from '@mui/icons-material/Loop';
+import React, { useState, useEffect, useContext, useRef } from "react";
+import {
+  Slider,
+  ToggleButtonGroup,
+  ToggleButton,
+  Snackbar,
+  Alert,
+} from "@mui/material";
+import LooksOneIcon from "@mui/icons-material/LooksOne";
+import LooksTwoIcon from "@mui/icons-material/LooksTwo";
+import Looks3Icon from "@mui/icons-material/Looks3";
+import Looks4Icon from "@mui/icons-material/Looks4";
+import Looks5Icon from "@mui/icons-material/Looks5";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import LoopIcon from "@mui/icons-material/Loop";
 
-import { getLanguageCode } from '../../utils/languageHelper';
+import { getLanguageCode } from "../../utils/languageHelper";
 
-import { userPreferenceContext } from '../../controllers/PreferenceController';
+import { userPreferenceContext } from "../../controllers/PreferenceController";
 
 function VocabPlaybackControl({ words }) {
+  const [selectedLevels, setSelectedLevels] = useState([
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+  ]);
+  const [playbackOn, setPlaybackOn] = useState(false);
+  const [loopOn, setLoopOn] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [filteredWords, setFilteredWords] = useState([]);
+  const [showLastWarning, setShowLastWarning] = useState(false);
+  const [showFirstWarning, setShowFirstWarning] = useState(false);
 
-    const [selectedLevels, setSelectedLevels] = useState(['1', '2', '3', '4', '5']);
-    const [playbackOn, setPlaybackOn] = useState(false);
-    const [loopOn, setLoopOn] = useState(false);
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [filteredWords, setFilteredWords] = useState([]);
-    const [showLastWarning, setShowLastWarning] = useState(false);
-    const [showFirstWarning, setShowFirstWarning] = useState(false);
+  const { userPreference, setUserPreference } = useContext(
+    userPreferenceContext
+  );
 
-    const { userPreference, setUserPreference } = useContext(userPreferenceContext);
+  const loopOnRef = useRef(loopOn);
+  const currentIndexRef = useRef(0);
 
-    const loopOnRef = useRef(loopOn);
-    const currentIndexRef = useRef(0);
+  // Initialize speech synthesis voices and utterance
+  const synth = window.speechSynthesis;
+  const targetLanguage = getLanguageCode(userPreference["target language"]);
+  const originalLanguage = getLanguageCode(userPreference["original language"]);
 
-    // Initialize speech synthesis voices and utterance
-    const synth = window.speechSynthesis;
-    const targetLanguage = getLanguageCode(userPreference["target language"]);
-    const originalLanguage = getLanguageCode(userPreference["original language"]);
+  const targetVoice = synth
+    .getVoices()
+    .find((voice) => voice.lang === targetLanguage);
+  const originalVoice = synth
+    .getVoices()
+    .find((voice) => voice.lang === originalLanguage);
 
-    const targetVoice = synth.getVoices().find(voice => voice.lang === targetLanguage);
-    const originalVoice = synth.getVoices().find(voice => voice.lang === originalLanguage);
+  const utterWord = new SpeechSynthesisUtterance();
+  utterWord.voice = targetVoice;
+  utterWord.rate = 0.7;
+  utterWord.pitch = 0.8;
+  const utterTranslation = new SpeechSynthesisUtterance();
+  utterTranslation.voice = originalVoice;
+  const utterExample = new SpeechSynthesisUtterance();
+  utterExample.voice = targetVoice;
+  utterWord.rate = 0.85;
 
-    const utterWord = new SpeechSynthesisUtterance();
-    utterWord.voice = targetVoice;
-    utterWord.rate = 0.7;
-    utterWord.pitch = 0.8;
-    const utterTranslation = new SpeechSynthesisUtterance();
-    utterTranslation.voice = originalVoice;
-    const utterExample = new SpeechSynthesisUtterance();
-    utterExample.voice = targetVoice;
-    utterWord.rate = 0.85;
- 
-    const onSelectedLevelChange = (_, newLevels) => {
-        setSelectedLevels(newLevels);
+  const onSelectedLevelChange = (_, newLevels) => {
+    setSelectedLevels(newLevels);
+  };
+
+  const onLoopChange = () => {
+    setLoopOn(!loopOn);
+    loopOnRef.current = !loopOn;
+  };
+
+  const onSliderChanged = (_, newIndex) => {
+    setCurrentIndex(newIndex);
+    currentIndexRef.current = newIndex;
+
+    // Cancel current utterance and start with the new word
+    synth.cancel();
+    readWord(filteredWords[currentIndexRef.current], currentIndexRef.current);
+  };
+
+  const onNextClick = () => {
+    var index = currentIndex;
+    if (index === filteredWords.length - 1) {
+      setShowLastWarning(true);
+      return;
     }
 
-    const onLoopChange = () => {
-        setLoopOn(!loopOn);
-        loopOnRef.current = !loopOn;
+    onSliderChanged(null, index + 1);
+  };
+
+  const onPrevClick = () => {
+    var index = currentIndex;
+    if (index === 0) {
+      setShowFirstWarning(true);
+      return;
     }
 
-    const onSliderChanged = (_, newIndex) => {
-        setCurrentIndex(newIndex);
-        currentIndexRef.current = newIndex;
+    onSliderChanged(null, index - 1);
+  };
 
-        // Cancel current utterance and start with the new word
-        synth.cancel();
-        readWord(filteredWords[currentIndexRef.current], currentIndexRef.current);
+  const togglePlayback = () => {
+    var playing = playbackOn;
+
+    setPlaybackOn(!playbackOn);
+
+    if (playing) {
+      synth.cancel();
+      return;
     }
 
-    const onNextClick = () => {
-        var index = currentIndex;
-        if (index === filteredWords.length - 1) {
-            setShowLastWarning(true);
-            return;
-        } 
+    readWord(filteredWords[currentIndex], currentIndex);
+  };
 
-        onSliderChanged(null, index + 1);
-    }
+  const readWord = (word, index) => {
+    var nextIndex = index + 1;
 
-    const onPrevClick = () => {
-        var index = currentIndex;
-        if (index === 0) {
-            setShowFirstWarning(true);
-            return;
-        } 
+    // Speak word three times, then translation, then example. Ensure each fragment is completed before starting next fragment.
+    utterWord.text = `${word.word}, ${word.word}, ${word.word}`;
+    utterWord.onend = () => {
+      utterTranslation.text = word.translation;
+      utterTranslation.onend = () => {
+        utterExample.text = word.example;
+        utterExample.onend = handleEndOfWord;
+        synth.speak(utterExample);
+      };
+      synth.speak(utterTranslation);
+    };
+    synth.speak(utterWord);
 
-        onSliderChanged(null, index - 1);
-    }
-
-    const togglePlayback = () => {
-        var playing = playbackOn;
-
-        setPlaybackOn(!playbackOn);
-
-        if (playing) {
-            synth.cancel();
-            return;
-        }
-        
-        readWord(filteredWords[currentIndex], currentIndex);
-    }
-
-    const readWord = (word, index) => {
-        var nextIndex = index + 1;
-
-        // Speak word three times, then translation, then example. Ensure each fragment is completed before starting next fragment.
-        utterWord.text = `${word.word}, ${word.word}, ${word.word}`;
-        utterWord.onend = () => {
-            utterTranslation.text = word.translation;
-            utterTranslation.onend = () => {
-                utterExample.text = word.example;
-                utterExample.onend = handleEndOfWord;
-                synth.speak(utterExample);
-            };
-            synth.speak(utterTranslation);
-        };
-        synth.speak(utterWord);
-    
-        // At the end of each word, start next word or return to the first word
-        const handleEndOfWord = () => {
-            if (nextIndex < filteredWords.length) {
-                setCurrentIndex(nextIndex);
-                readWord(filteredWords[nextIndex], nextIndex);
-            } else {
-                setCurrentIndex(0);
-                if (loopOnRef.current) {
-                    readWord(filteredWords[0], 0);
-                } else {
-                    setPlaybackOn(false);
-                }
-            }
-        }
-    }
-
-    const valueLabelFormat = (value) => {
-        if (filteredWords.length === 0) {
-            return words[0].word;
+    // At the end of each word, start next word or return to the first word
+    const handleEndOfWord = () => {
+      if (nextIndex < filteredWords.length) {
+        setCurrentIndex(nextIndex);
+        readWord(filteredWords[nextIndex], nextIndex);
+      } else {
+        setCurrentIndex(0);
+        if (loopOnRef.current) {
+          readWord(filteredWords[0], 0);
         } else {
-            return filteredWords[value].word;
+          setPlaybackOn(false);
         }
+      }
+    };
+  };
+
+  const valueLabelFormat = (value) => {
+    if (filteredWords.length === 0) {
+      return words[0].word;
+    } else {
+      return filteredWords[value].word;
     }
+  };
 
-    useEffect(() => {
-        var newWords = words.filter(word => selectedLevels.includes(word.level));
-        setFilteredWords(newWords);
-    }, [words, selectedLevels]);
+  useEffect(() => {
+    var newWords = words.filter((word) => selectedLevels.includes(word.level));
+    setFilteredWords(newWords);
+  }, [words, selectedLevels]);
 
-    return (
-        <div id='playback-controller'>
-            <div id='level-filter-container'>
-                <span id='level-filter-title'>Level Filter</span>
-                <ToggleButtonGroup 
-                    value={selectedLevels} 
-                    onChange={onSelectedLevelChange} 
-                    sx={{ gap: '5px' }}
-                    disabled={playbackOn}
-                >
-                    <ToggleButton value='1' aria-label='1' sx={{ padding: '5px' }}>
-                        <LooksOneIcon />
-                    </ToggleButton>
-                    <ToggleButton value='2' aria-label='2' sx={{ padding: '5px' }}>
-                        <LooksTwoIcon />
-                    </ToggleButton>
-                    <ToggleButton value='3' aria-label='3' sx={{ padding: '5px' }}>
-                        <Looks3Icon />
-                    </ToggleButton>
-                    <ToggleButton value='4' aria-label='4' sx={{ padding: '5px' }}>
-                        <Looks4Icon />
-                    </ToggleButton>
-                    <ToggleButton value='5' aria-label='5' sx={{ padding: '5px' }}>
-                        <Looks5Icon />
-                    </ToggleButton>
-                </ToggleButtonGroup>
-            </div>
-            <Slider 
-                value={currentIndex}
-                max={filteredWords.length - 1}
-                onChange={onSliderChanged}
-                valueLabelDisplay="on"
-                valueLabelFormat={valueLabelFormat}
-                sx={{ paddingTop: '8px' }}
-            />
-            <div id='playback-control-button-group'>
-                <button onClick={onPrevClick}>
-                    <ArrowLeftIcon />
-                </button>
-                <button onClick={togglePlayback}>
-                    {playbackOn ? <PauseIcon /> : <PlayArrowIcon />}
-                </button>
-                <button onClick={onNextClick}>
-                    <ArrowRightIcon />
-                </button>
-                <ToggleButton 
-                    value="loop" 
-                    selected={loopOn} 
-                    id='playback-loop-button' 
-                    onClick={onLoopChange} 
-                    sx={{ padding: '0px' }}
-                >
-                    <LoopIcon />
-                </ToggleButton>
-            </div>
-            <Snackbar
-                open={showFirstWarning}
-                autoHideDuration={3000}
-                onClose={() => { setShowFirstWarning(false); }}
-            >
-                <Alert 
-                    severity='warning'
-                    sx={{ backgroundColor: '#aacdfa', fontFamily: 'Catamaran', borderRadius: '20px' }}
-                >
-                    This is already the first word!
-                </Alert>
-            </Snackbar>
-            <Snackbar
-                open={showLastWarning}
-                autoHideDuration={3000}
-                onClose={() => { setShowLastWarning(false); }}
-            >
-                <Alert 
-                    severity='warning' 
-                    sx={{ backgroundColor: '#aacdfa', fontFamily: 'Catamaran', borderRadius: '20px' }}
-                >
-                    This is already the last word!
-                </Alert>
-            </Snackbar>
-        </div>
-    )
+  return (
+    <div id="playback-controller">
+      <div id="level-filter-container">
+        <span id="level-filter-title">Level Filter</span>
+        <ToggleButtonGroup
+          value={selectedLevels}
+          onChange={onSelectedLevelChange}
+          sx={{ gap: "5px" }}
+          disabled={playbackOn}
+        >
+          <ToggleButton value="1" aria-label="1" sx={{ padding: "5px" }}>
+            <LooksOneIcon />
+          </ToggleButton>
+          <ToggleButton value="2" aria-label="2" sx={{ padding: "5px" }}>
+            <LooksTwoIcon />
+          </ToggleButton>
+          <ToggleButton value="3" aria-label="3" sx={{ padding: "5px" }}>
+            <Looks3Icon />
+          </ToggleButton>
+          <ToggleButton value="4" aria-label="4" sx={{ padding: "5px" }}>
+            <Looks4Icon />
+          </ToggleButton>
+          <ToggleButton value="5" aria-label="5" sx={{ padding: "5px" }}>
+            <Looks5Icon />
+          </ToggleButton>
+        </ToggleButtonGroup>
+      </div>
+      <Slider
+        value={currentIndex}
+        max={filteredWords.length - 1}
+        onChange={onSliderChanged}
+        valueLabelDisplay="on"
+        valueLabelFormat={valueLabelFormat}
+        sx={{ paddingTop: "8px" }}
+      />
+      <div id="playback-control-button-group">
+        <button onClick={onPrevClick}>
+          <ArrowLeftIcon />
+        </button>
+        <button onClick={togglePlayback}>
+          {playbackOn ? <PauseIcon /> : <PlayArrowIcon />}
+        </button>
+        <button onClick={onNextClick}>
+          <ArrowRightIcon />
+        </button>
+        <ToggleButton
+          value="loop"
+          selected={loopOn}
+          id="playback-loop-button"
+          onClick={onLoopChange}
+          sx={{ padding: "0px" }}
+        >
+          <LoopIcon />
+        </ToggleButton>
+      </div>
+      <Snackbar
+        open={showFirstWarning}
+        autoHideDuration={3000}
+        onClose={() => {
+          setShowFirstWarning(false);
+        }}
+      >
+        <Alert
+          severity="warning"
+          sx={{ backgroundColor: "#aacdfa", borderRadius: "20px" }}
+        >
+          This is already the first word!
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={showLastWarning}
+        autoHideDuration={3000}
+        onClose={() => {
+          setShowLastWarning(false);
+        }}
+      >
+        <Alert
+          severity="warning"
+          sx={{ backgroundColor: "#aacdfa", borderRadius: "20px" }}
+        >
+          This is already the last word!
+        </Alert>
+      </Snackbar>
+    </div>
+  );
 }
 
-export default VocabPlaybackControl
+export default VocabPlaybackControl;
